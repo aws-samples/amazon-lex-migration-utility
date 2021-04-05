ce4a9dc9b2035a09de30aea34696ff142a9b14e7
Karl Thomas <thokrl@amazon.com>
2021-04-05 16:00:00 +0000

    Update create_lex_template.py

--- exporter/create_lex_template.py
+++ exporter/create_lex_template.py
@@ -100,7 +100,7 @@ with open("./config_schema.json","r") as file:
 try:
     jsonschema.validate(instance=config,schema = schema )
 except:
-    print(f"{sys.argv[2]} is not valid config file.")
+    print(f"{config_file} is not valid config file.")
     sys.exit(show_help(True))
 
 
